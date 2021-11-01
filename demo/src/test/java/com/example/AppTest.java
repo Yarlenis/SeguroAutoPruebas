3980fb8b36ab4bc107d9e4f5eef606755f2019b9
Yarlenis Alabarca <Yarlenis.alabarca@utp.ac.pa>
2021-11-01 17:28:35 -0500

    subiendo SeguroAutoPruebas

--- demo/src/test/java/com/example/AppTest.java
+++ demo/src/test/java/com/example/AppTest.java
@@ -16,15 +16,15 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class AppTest 
 {
     private WebDriver driver;
-    By edadLocator = By.name("edad");
-    By generoLocator = By.name("genero");
-    By btnLocator = By.name("btn_consultar");
+    By edadLocalizador = By.name("edad");
+    By generoLocalizador = By.name("genero");
+    By botonLocalizador = By.name("btn_consultar");
     By bodySelector = By.cssSelector("body");
 
     @Before
     public void setUp()
     {
-        System.setProperty("webdriver.chrome.driver","C:/Users/Gabriel/Desktop/SegurosAutoPruebas/demo/src/test/resources/chromedriver/chromedriver.exe");
+        System.setProperty("webdriver.chrome.driver","C:/Users/YarlenisAideth/Desktop/SegurosAutoPruebas/demo/src/test/resources/chromedriver/chromedriver.exe");
         driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.get("http://localhost");
@@ -34,9 +34,9 @@ public class AppTest
     public void testConsultarPrimaAnual() throws InterruptedException
     {
         Thread.sleep(1000);
-        driver.findElement(generoLocator).sendKeys("masculino");
-        driver.findElement(edadLocator).sendKeys("18");
-        driver.findElement(btnLocator).click();
+        driver.findElement(generoLocalizador).sendKeys("masculino");
+        driver.findElement(edadLocalizador).sendKeys("18");
+        driver.findElement(botonLocalizador).click();
         assertEquals("El valor de la prima anual de seguro de auto según su género y edad es: 2000 dólares", driver.findElement(bodySelector).getText());
     }
 
